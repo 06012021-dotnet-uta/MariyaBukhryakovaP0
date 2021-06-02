814d0336ea811a16edcab93f90a33713dfd698a3
Mariya Bukhryakova <mariya.bukhryakova@revature.net>
2021-06-01 23:19:18 -0400

    added input request and writeline statement

---
+++ helloworld/hello-world/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Change it as you want");
+
+            
+
+            Console.WriteLine($"Enter your age:");
+            string myAge = Console.ReadLine ();
+
+            Console.WriteLine($"Enter your name:");
+            string myName = Console.ReadLine();
+
+            Console.WriteLine($"Your age is {myAge} and your name is {myName}");
+
+
+        }
+    }
+}
