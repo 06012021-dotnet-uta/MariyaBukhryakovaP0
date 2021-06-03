840de98464d66007a81af8c77f16dc6907cdd13f
Mariya Bukhryakova <mariya.bukhryakova@revature.net>
2021-06-03 12:03:08 -0400

    moved enum to partial class

---
+++ rockpaperscissors/rockpaperscissorsGame/RPSChoices.cs
@@ -0,0 +1,11 @@
+namespace rockpaperscissorsGame
+{
+    public enum RPSChoices
+    {
+        //creates a variable that I set to values
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3
+        
+    }
+}// end of namespace
